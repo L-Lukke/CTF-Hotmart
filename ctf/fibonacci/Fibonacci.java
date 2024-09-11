ad15471420eb929db27aadfb91a21fc259b34a04
Lukke <111764699+L-Lukke@users.noreply.github.com>
2024-09-11 12:11:50 -0300

    Delete ctf directory

--- ctf/fibonacci/Fibonacci.java
+++
@@ -1,6 +0,0 @@
-public class Fibonacci {
-    public static void main(String[] args) {
-        int[] fibonacciSequence = {2584, 4181};
-        System.out.println(fibonacciSequence[0] +  fibonacciSequence[1]);
-    }
-}
